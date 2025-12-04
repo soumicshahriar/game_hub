b6e6ed608a5993eeb8e03f5c3b7056af7c836a9d
soumicshahriar <soumicshahriar1@gmail.com>
2025-12-04 12:55:46 +0600

    add sorting to the reccomended games page

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -3,14 +3,25 @@ import { useLoaderData } from "react-router";
 import { motion } from "motion/react";
 import { cardVariants, containerVariants } from "../motion/gamesMotion";
 import useTitle from "../utils/useTitle";
+import { useState } from "react";
 
 const Recommended = () => {
   useTitle("Recommended Games");
   const games = useLoaderData();
 
+  // sort functionality
+  const [sortOrder, setSortOrder] = useState("desc");
+
   const recommendedGames = [...games]
     .sort((a, b) => parseFloat(b.ratings) - parseFloat(a.ratings))
     .slice(3, 15);
+
+  // apply sorting based on dropdown sorting
+  const sortedRecommended = [...recommendedGames].sort((a, b) => {
+    const ratingA = parseFloat(a.ratings);
+    const ratingB = parseFloat(b.ratings);
+    return sortOrder === "asc" ? ratingA - ratingB : ratingB - ratingA;
+  });
 
   return (
     <section className="mt-15 px-4">
@@ -18,13 +29,29 @@ const Recommended = () => {
         🌟 Recommended <span className="text-[#ffd166]">Games</span>
       </h2>
 
+      {/* Sorting Dropdown */}
+      <div className="flex justify-end mb-6">
+        <select
+          value={sortOrder}
+          onChange={(e) => setSortOrder(e.target.value)}
+          className="px-4 py-2 border border-gray-300 rounded-md bg-transparent text-white focus:outline-none focus:ring focus:ring-[#ffd166]"
+        >
+          <option value="desc" className="text-black">
+            Rating: High → Low
+          </option>
+          <option value="asc" className="text-black">
+            Rating: Low → High
+          </option>
+        </select>
+      </div>
+
       <motion.div
         className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
         variants={containerVariants}
         initial="hidden"
         whileInView="show"
       >
-        {recommendedGames.map((game) => (
+        {sortedRecommended.map((game) => (
           <motion.div
             key={game.id}
             variants={cardVariants}
